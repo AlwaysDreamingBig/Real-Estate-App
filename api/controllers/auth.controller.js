41a287a1ff798d73a361c7c02b48d0e0f12da620
AlwaysDreamingBig <167980440+AlwaysDreamingBig@users.noreply.github.com>
2024-06-04 13:10:18 +0200

    Add middleware and function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,14 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
-    console.log(req.body);
+export const signup = async (req, res, next) => {
+    //console.log(req.body);
 
     const {username, email, password} = req.body;
 
     if(!username || !email || !password || username ==='' || email === '' || password === ''){
-        return res.status(400).json({ message: 'All fields are required'});
+        next(errorHandler(400, 'All fields are required'));
     } 
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -23,8 +24,9 @@ export const signup = async (req, res) => {
     try{
         await newUser.save();
         res.json('SignUp succesful');
-    }catch(err){
-        res.status(500).json({ message: err.message} );
+    }catch(error){
+        //res.status(500).json({ message: error.message} );
+        next(error);
     }
     
 }
--- api/index.js
+++ api/index.js
@@ -30,3 +30,19 @@ app.listen(3000, () => {
         
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+
+            {/** Middleware to handle errors
+                err is is the error goot from the input
+                req is the request
+                res is the response
+                next is the var used to get to the next middleware */}
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
