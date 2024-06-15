f7ec4479a80c65de02d380059dd86816300ad004
AlwaysDreamingBig <167980440+AlwaysDreamingBig@users.noreply.github.com>
2024-06-15 09:25:19 +0200

    Create Sign-in API Route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
 import { verifyPassword } from "../utils/utility.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     //console.log(req.body);
@@ -35,3 +36,44 @@ export const signup = async (req, res, next) => {
     }
     
 };
+
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password || email === '' || password === ''){
+        next(errorHandler(400, 'All fields are required'));
+    }
+
+    try {
+    
+        const validUser = await User.findOne({email: email});
+
+        if(!validUser){
+            return next(errorHandler(404, 'User not found.'));
+        }else{
+            const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+            if(!validPassword){
+                return next(errorHandler(405, 'Wrong Credentials'))
+            }
+        }
+
+        {/** Creating the jwt token for auth,
+            _id is the id set by MongoDB automatically
+            process.env.JWT_SECRET is our secret key
+            */}
+
+        const token = jwt.sign({ id:validUser._id }, process.env.JWT_SECRET);
+            //To prevent leak of the password when the function returns the user info: separate the password from the rest of the user info
+        const { password:pass, ...rest } = validUser._doc;
+
+        //res.cookie('access_token', token, {httpOnly:true, expires: new Date(Date.now() + 24 * 60 * 60)});
+        res.cookie('access_token', token, { httpOnly:true })
+            .status(200).json(rest);
+        
+        
+    } catch (error) {
+        next(error);
+    }
+}
