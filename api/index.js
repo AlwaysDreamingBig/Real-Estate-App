646028397eea9893d4e9fe59e13a6d42826a7c06
AlwaysDreamingBig <167980440+AlwaysDreamingBig@users.noreply.github.com>
2024-06-22 21:50:23 +0200

    Listing Router & createListing API route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 
         {/** Database connexion */}
@@ -39,6 +40,7 @@ app.use(cookieParser());
         
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/listing', listingRouter);
 
 
             {/** Middleware to handle errors
