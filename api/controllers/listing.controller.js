082bc4418e3d39ca70d659b861184665824158c3
AlwaysDreamingBig <167980440+AlwaysDreamingBig@users.noreply.github.com>
2024-07-03 10:27:28 +0200

    Get a Listing Info API Route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -94,3 +94,21 @@ export const updateListing = async (req, res, next) => {
       next(error)
   }
 };
+
+export const getListing = async (req, res, next) => {
+
+  try {
+
+        // Ensure the listing belongs to the user making the request
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing){
+      return next(errorHandler(404, 'Listing was not found!'));
+    }
+
+    return res.status(200).json(listing);
+ 
+  } catch (error) {
+      next(error)
+  }
+};
