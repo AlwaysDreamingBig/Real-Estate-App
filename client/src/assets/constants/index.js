aecb0d450c03d6686d5681cea333ce261bd50aad
AlwaysDreamingBig <167980440+AlwaysDreamingBig@users.noreply.github.com>
2024-07-04 14:14:36 +0200

    Create constants folder

---
+++ client/src/assets/constants/index.js
@@ -0,0 +1,11 @@
+export const initialMessages = [
+    { id: 1, sender: "Alice", text: "Hello! How are you today?", timestamp: "10:00 AM", read: true },
+    { id: 2, sender: "Bob", text: "What are your plans for the weekend?", timestamp: "10:05 AM", read: false },
+    { id: 3, sender: "Charlie", text: "I heard the weather will be nice tomorrow.", timestamp: "10:10 AM", read: true },
+    { id: 4, sender: "Alice", text: "Hello! How are you today?", timestamp: "10:00 AM", read: true },
+    { id: 5, sender: "Bob", text: "What are your plans for the weekend?", timestamp: "10:05 AM", read: false },
+    { id: 6, sender: "Charlie", text: "I heard the weather will be nice tomorrow.", timestamp: "10:10 AM", read: true },
+    { id: 7, sender: "Alice", text: "Hello! How are you today?", timestamp: "10:00 AM", read: true },
+    { id: 8, sender: "Bob", text: "What are your plans for the weekend?", timestamp: "10:05 AM", read: false },
+    { id: 9, sender: "Charlie", text: "I heard the weather will be nice tomorrow.", timestamp: "10:10 AM", read: true },
+  ];
