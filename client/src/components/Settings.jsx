4da7aad27b49eda340df85387f86687dc79f2bd8
AlwaysDreamingBig <167980440+AlwaysDreamingBig@users.noreply.github.com>
2024-07-08 15:35:47 +0200

    Add handleChange form Data to the Settings page

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -1,6 +1,4 @@
-// Settings.js
-
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { 
   faBell, 
@@ -16,7 +14,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserFailure, signOutUserStart, signOutUserSuccess } from '../redux/user/userSlice.js';
 import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 
@@ -25,7 +23,25 @@ const Settings = () => {
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const {currentUser} = useSelector((state) => state.user)
+  const { currentUser } = useSelector((state) => state.user);
+
+  // State for notification preferences
+  const [emailNotifications, setEmailNotifications] = useState(currentUser.settings.emailNotifications);
+
+  const [messageNotifications, setMessageNotifications] = useState(currentUser.settings.messageNotifications);
+
+  // State for public profile
+  const [publicProfile, setPublicProfile] = useState(currentUser.settings.publicProfile);
+
+  // State for data sharing
+  const [dataSharing, setDataSharing] = useState(currentUser.settings.dataSharing);
+
+  // State for language preference
+  const [language, setLanguage] = useState(currentUser.settings.language);
+
+  // State for currency preference
+  const [currency, setCurrency] = useState(currentUser.settings.currency);
+
 
 
   const handleDeleteUser = async () => {
@@ -88,13 +104,42 @@ const Settings = () => {
           dispatch(signOutUserSuccess(data));
     
           //If everything is alright navigate to the Sign(in page
-         // navigate('/signin');
+          navigate('/signin');
   
     } catch (error) {
         dispatch(signOutUserFailure(error.message));
     }
   };
   
+
+
+  const handleChange = (event) => {
+    const { name, value, type, checked } = event.target;
+
+    switch (name) {
+      case 'emailNotifications':
+        setEmailNotifications(checked);
+        break;
+      case 'messageNotifications':
+        setMessageNotifications(checked);
+        break;
+      case 'publicProfile':
+        setPublicProfile(checked);
+        break;
+      case 'dataSharing':
+        setDataSharing(checked);
+        break;
+      case 'language':
+        setLanguage(value);
+        break;
+      case 'currency':
+        setCurrency(value);
+        break;
+      default:
+        break;
+    }
+  };
+
 
   return (
     <div className="max-w-lg mx-auto p-8 bg-white rounded-lg shadow-lg max-h-screen overflow-y-auto">
@@ -110,14 +155,28 @@ const Settings = () => {
           <div>
             <label className="block mb-1">Email Notifications</label>
             <div className="flex items-center">
-              <input type="checkbox" id="emailNotifications" className="mr-2" />
+              <input
+                type="checkbox"
+                id="emailNotifications"
+                name="emailNotifications"
+                checked={emailNotifications}
+                onChange={handleChange}
+                className="mr-2"
+              />
               <label htmlFor="emailNotifications">Receive email notifications</label>
             </div>
           </div>
           <div className="ml-8">
             <label className="block mb-1">Message Notifications</label>
             <div className="flex items-center">
-              <input type="checkbox" id="messageNotifications" className="mr-2" />
+              <input
+                type="checkbox"
+                id="messageNotifications"
+                name="messageNotifications"
+                checked={messageNotifications}
+                onChange={handleChange}
+                className="mr-2"
+              />
               <label htmlFor="messageNotifications">Receive SMS notifications</label>
             </div>
           </div>
@@ -136,7 +195,7 @@ const Settings = () => {
               <button className="text-red-600 hover:underline focus:outline-none" onClick={handleDeleteUser}>
                 <FontAwesomeIcon icon={faTrashAlt} className="text-gray-500 mr-3 text-sm" />
                 Delete account
-            </button>
+              </button>
             </div>
           </div>
           <div className="flex items-center">
@@ -144,7 +203,7 @@ const Settings = () => {
               <button className="text-red-600 hover:underline focus:outline-none" onClick={handleSignOutUser}>
                 <FontAwesomeIcon icon={faSignOutAlt} className="text-gray-500 mr-3 text-sm" />
                 Sign Out
-            </button>
+              </button>
             </div>
           </div>
         </div>
@@ -160,7 +219,14 @@ const Settings = () => {
           <div>
             <label className="block mb-1">Visible Information</label>
             <div className="flex items-center">
-              <input type="checkbox" id="publicProfile" className="mr-2" />
+              <input
+                type="checkbox"
+                id="publicProfile"
+                name="publicProfile"
+                checked={publicProfile}
+                onChange={handleChange}
+                className="mr-2"
+              />
               <label htmlFor="publicProfile">Show listings viewed and favorites on public profile</label>
             </div>
           </div>
@@ -177,7 +243,14 @@ const Settings = () => {
           <div>
             <label className="block mb-1">Share Data</label>
             <div className="flex items-center">
-              <input type="checkbox" id="dataSharing" className="mr-2" />
+              <input
+                type="checkbox"
+                id="dataSharing"
+                name="dataSharing"
+                checked={dataSharing}
+                onChange={handleChange}
+                className="mr-2"
+              />
               <label htmlFor="dataSharing">Share my data with third parties or affiliates</label>
             </div>
           </div>
@@ -193,7 +266,13 @@ const Settings = () => {
         <div className="flex items-center mb-4">
           <div>
             <label className="block mb-1">Language</label>
-            <select className="border rounded-lg p-2 outline-none">
+            <select
+              className="border rounded-lg p-2 outline-none"
+              id="language"
+              name="language"
+              value={language}
+              onChange={handleChange}
+            >
               <option value="en">English</option>
               <option value="fr">French</option>
               <option value="es">Spanish</option>
@@ -206,7 +285,13 @@ const Settings = () => {
                 <FontAwesomeIcon icon={faMoneyBillWave} className="text-gray-500 mr-3" />
                 Currency
             </label>
-            <select className="border rounded-lg p-2 outline-none">
+            <select
+              className="border rounded-lg p-2 outline-none"
+              id="currency"
+              name="currency"
+              value={currency}
+              onChange={handleChange}
+            >
               <option value="usd">USD - US Dollar</option>
               <option value="eur">EUR - Euro</option>
               <option value="gbp">GBP - British Pound</option>
