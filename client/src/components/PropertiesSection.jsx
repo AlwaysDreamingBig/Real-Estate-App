27c0b83ea411b26ac95b6cfd136da9ae52a62f0d
AlwaysDreamingBig <167980440+AlwaysDreamingBig@users.noreply.github.com>
2024-08-11 18:45:25 +0200

    Home Page: Adjust Properties section

--- client/src/components/PropertiesSection.jsx
+++ client/src/components/PropertiesSection.jsx
@@ -4,7 +4,10 @@ import ListingCard from './ListingPreview';
 const PropertiesSection = ({ listings }) => {
   return (
     <div className="container mx-auto px-4 mt-10">
-      <h2 className="text-4xl font-serif font-semibold text-gray-600 text-start ml-8 mb-8">Featured Listings</h2>
+      <h2 className="text-4xl font-serif font-semibold text-black text-center mb-2">Featured Listings</h2>
+      <p className="text-lg text-gray-600 text-center  mb-8">
+            Let's have a look on some of our multiple Advidor's properties.
+      </p>
 
       <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
         {listings.map((property, index) => (
